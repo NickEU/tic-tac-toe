69fc82c163aeff5ac8f3c819a156befe8a892af3
Nick <neverate@gmail.com>
2020-02-26 16:50:58 +0200

    + X marks the spot

--- src/App.js
+++ src/App.js
@@ -1,7 +1,75 @@
 import React from "react";
 
+class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      label: this.props.value
+    };
+  }
+  handleClick = () => {
+    this.setState(_state => {
+      return { label: "X" };
+    });
+  };
+  render() {
+    return (
+      <button className='square' onClick={this.handleClick}>
+        {this.state.label}
+      </button>
+    );
+  }
+}
+
+class Board extends React.Component {
+  renderSquare(i) {
+    return <Square value={i} />;
+  }
+
+  render() {
+    const status = "Next player: X";
+
+    return (
+      <div>
+        <div className='status'>{status}</div>
+        <div className='board-row'>
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className='board-row'>
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className='board-row'>
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className='game'>
+        <div className='game-board'>
+          <Board />
+        </div>
+        <div className='game-info'>
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
 const App = () => {
-  return <div>Tic-tac-toe!</div>;
+  return <Game />;
 };
 
 export default App;
